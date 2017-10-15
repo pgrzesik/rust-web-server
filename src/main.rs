7a3f19fddb2a9fbae83f090559e92d7520fe1892
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-15 23:50:05 +0200

    Add API for thread pool

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(8);
 
     println!("Start listening...");
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
