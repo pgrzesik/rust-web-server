d52bd6d941a62b7d8e47f0bb092d3d157113a1d8
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-15 23:10:09 +0200

    Add listening for incoming streams

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    println!("Start listening...");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("New connection!");
+    }
 }
