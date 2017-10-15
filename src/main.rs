0df731ea1be41fb648fc011aeca9181a2af7cce6
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-15 23:33:24 +0200

    Add returning 404 if asking for resource other than /

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,15 @@ fn handle_connection(mut stream: TcpStream) {
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     } else {
-        println!("Different request!");
-    }
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let mut file = File::open("html/404.html").unwrap();
 
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
